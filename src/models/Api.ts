bf968f745f650ce247d8c795db19f25acb8bdaa5
RienNeVaPlus <?@rienneva.plus>
2020-08-24 01:33:39 +0200

    optimizes cookie usage implements an authorization timeout (12h) improved errors

--- src/models/Api.ts
+++ src/models/Api.ts
@@ -10,8 +10,8 @@ interface Options {
 	email: string
 	password: string
 	locale?: [string, string]
-	cookies?: any[]
 	defaults?: Defaults
+	timeout?: number
 }
 
 interface Defaults {
@@ -20,9 +20,12 @@ interface Defaults {
 
 interface Opt extends Options {
 	locale: [string, string]
-	cookies: any[]
+	cookie?: string
+	timeout: number
 	defaults: Defaults
 }
+
+let timeout: NodeJS.Timeout;
 
 export class Api {
 	static url = 'https://www.wikifolio.com/';
@@ -32,7 +35,7 @@ export class Api {
 	constructor(options: Options){
 		this.opt = {
 			locale: ['de', 'de'],
-			cookies: [],
+			timeout: 60 * 60 * 12,
 			...options,
 			defaults: {
 				pageSize: 50,
@@ -41,11 +44,11 @@ export class Api {
 		};
 	}
 
-	private async auth(): Promise<any[]> {
-		let { opt: {cookies} } = this;
+	private async auth(): Promise<boolean> {
+		let { opt: {cookie} } = this;
 
-		if(cookies.length)
-			return cookies;
+		if(cookie)
+			return false;
 
 		const {email, password} = this.opt;
 
@@ -56,7 +59,6 @@ export class Api {
 
 		// request form for session vars
 		const form = await request({url, resolveWithFullResponse:true});
-		cookies = form.headers['set-cookie'] || [];
 
 		const {window: {document}} = new JSDOM(form.body);
 		const __RequestVerificationToken = (document.querySelector('[name=__RequestVerificationToken]') as HTMLInputElement).value;
@@ -74,13 +76,18 @@ export class Api {
 			resolveWithFullResponse: true
 		});
 
-		if(!res.headers['set-cookie'])
+		if(!res.request.headers['cookie'])
 			throw new Error('Login failed, Cookie not found');
 
-		cookies = this.opt.cookies = [...cookies, ...res.headers['set-cookie']];
-		cookies.map(cookie => jar.setCookie(cookie, Api.url));
+		this.opt.cookie = res.request.headers['cookie'];
 
-		return cookies;
+		if(timeout) clearTimeout(timeout);
+		timeout = setTimeout(
+			() => this.opt.cookie = undefined,
+			this.opt.timeout * 1000
+		);
+
+		return true;
 	}
 
 	public async request(arg: string | RequestOpt, authorize: boolean = true, fullResponse: boolean = false): Promise<any> {
@@ -97,7 +104,9 @@ export class Api {
 		let res = await request(options);
 
 		if(!fullResponse && typeof res === 'string' && String(options.url).includes('/api/')){
-			try { res = JSON.parse(res); } catch(e){}
+			try { res = JSON.parse(res); } catch(e){ throw new Error('Invalid JSON'); }
+			if(res.message === 'Authorization has been denied for this request.')
+				throw new Error(res.message);
 		}
 
 		return res;
